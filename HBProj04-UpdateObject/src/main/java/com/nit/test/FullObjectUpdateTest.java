c12ffc77472fb8863ed0e998d41d94b28f59f15f
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-21 12:41:31 +0530

    HBProj04-UpdateObject

---
+++ HBProj04-UpdateObject/src/main/java/com/nit/test/FullObjectUpdateTest.java
@@ -0,0 +1,54 @@
+package com.nit.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.nit.entity.Product;
+
+public class FullObjectUpdateTest {
+
+	public static void main(String[] args) {
+
+		//BootStrap / Activate the Hibernate
+		Configuration cfg = new Configuration();
+
+		//Specify the hibernate configuration file name and location
+		cfg.configure("com/nit/cfgs/hibernate.cfg.xml");
+
+		//Build SessionFactory object
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		//Create Session object
+		Session ses = factory.openSession();
+
+		Transaction tx = null;
+		try(factory;ses){  //java9
+			//Begin Tx
+			tx = ses.beginTransaction();
+
+			//Prepare object for entity class
+			Product prod = new Product();
+			prod.setPid(1012);  //must be existing id
+			prod.setPname("ShoeRack");  //new value
+			prod.setPrice(5000.78f);  //new value
+			prod.setQty(2.0f);  //new value
+
+			//instruction to update object
+			ses.update(prod);
+
+			//commit the tx
+			tx.commit();
+			System.out.println("Full object is updated");
+		}
+		catch(HibernateException he) {
+			if(tx!=null && tx.getStatus()!=null && tx.getRollbackOnly()) {
+				tx.rollback();
+				System.out.println("Object is not updated");
+			}
+			he.printStackTrace();
+		}
+	}//main
+}//class
