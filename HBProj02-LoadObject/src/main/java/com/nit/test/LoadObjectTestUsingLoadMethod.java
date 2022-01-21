b5f6210bc07a956ea119529e29eab1ce95fbf1f8
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-21 10:59:40 +0530

    HBProj02-LoadObject

---
+++ HBProj02-LoadObject/src/main/java/com/nit/test/LoadObjectTestUsingLoadMethod.java
@@ -0,0 +1,41 @@
+package com.nit.test;
+
+import java.util.Arrays;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.nit.entity.Product;
+
+public class LoadObjectTestUsingLoadMethod {
+
+	public static void main(String[] args) {
+
+		//BootStrap / Activate the Hibernate
+		Configuration cfg = new Configuration();
+
+		//Specify the hibernate configuration file name and location
+		cfg.configure("com/nit/cfgs/hibernate.cfg.xml");
+
+		//Build SessionFactory object
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		//Create Session object
+		Session ses = factory.openSession();
+
+		try(factory;ses){  //java9
+			//Load object
+			Product prod = ses.load(Product.class, 1013);
+			System.out.println(prod.getPid()+" "+prod.getPname()+" "+prod.getPrice()+" "+prod.getQty());
+			System.out.println(Arrays.toString(prod.getClass().getDeclaredMethods()));
+			System.out.println("---------------------------");
+			System.out.println(prod.getClass()+" "+prod.getClass().getSuperclass());
+		}
+		catch(HibernateException he) {
+			System.out.println("Record not found");
+			he.printStackTrace();
+		}
+	}//main
+}//class
