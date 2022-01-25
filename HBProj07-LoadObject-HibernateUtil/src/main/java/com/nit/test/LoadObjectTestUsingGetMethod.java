f4b12b6c30c5bbbf077ccc03bd38d8ae56b23bad
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-25 10:02:14 +0530

    HBProj07-LoadObject-HibernateUtil

---
+++ HBProj07-LoadObject-HibernateUtil/src/main/java/com/nit/test/LoadObjectTestUsingGetMethod.java
@@ -0,0 +1,30 @@
+package com.nit.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.nit.entity.Product;
+import com.nit.utility.HibernateUtil;
+
+public class LoadObjectTestUsingGetMethod {
+
+	public static void main(String[] args) {
+		//get SessionFactory object
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		//get Session object
+		Session ses = HibernateUtil.getSession();
+		try(factory;ses){  //java9
+			//Load object
+			Product prod = ses.get(Product.class, 1013);
+			if(prod==null)
+				System.out.println("Product not found");
+			else
+				System.out.println(prod);
+		}
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+	}//main
+}//class
