6db0996704ee78d4c39dc8b24d454804329c0ae6
deepa <deepa@DESKTOP-AJQTNNR>
2022-02-02 12:28:56 +0530

    HBProj12-Versioning

---
+++ HBProj12-Versioning/src/main/java/com/nit/entity/CallerTune.java
@@ -0,0 +1,11 @@
+package com.nit.entity;
+
+import lombok.Data;
+
+@Data
+public class CallerTune {
+	private Integer tuneId;
+	private String tuneName;
+	private String movieName;
+	private Integer updationCount;  //for versioning
+}
