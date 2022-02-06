7a6fe08ee1ed4dd5ce2d4886e1beac88fff0c474
deepa <deepa@DESKTOP-AJQTNNR>
2022-02-06 15:30:53 +0530

    HBProj15-Versioning-Timestamping-Anno

---
+++ HBProj15-Versioning-Timestamping-Anno/src/main/java/com/nit/test/SaveObjectTest.java
@@ -0,0 +1,40 @@
+package com.nit.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.nit.entity.CallerTune;
+import com.nit.utility.HibernateUtil;
+
+public class SaveObjectTest {
+
+	public static void main(String[] args) {
+		
+		//Get SessionFactory object
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		//Get Session object
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = null;
+		try(factory;ses){
+			//begin transaction
+			tx = ses.beginTransaction();
+			//Prepare object
+			CallerTune tune = new CallerTune();
+			tune.setTuneName("Srivalli");
+			tune.setMovieName("Pushpa:The Rise");
+			//Save object
+			ses.save(tune);
+			tx.commit();
+			System.out.println("Object is saved at :: "+tune.getLaunchTime());
+		}
+		catch(HibernateException he) {
+			if(tx!=null || tx.getStatus()!=null  || tx.getRollbackOnly()) {
+				tx.rollback();
+				System.out.println("Object is not saved");
+			}//if
+			he.printStackTrace();
+		}//catch
+	}//main
+}//class
---
+++ HBProj15-Versioning-Timestamping-Anno/src/main/java/com/nit/test/UpdateObjectTest.java
@@ -0,0 +1,46 @@
+package com.nit.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.nit.entity.CallerTune;
+import com.nit.utility.HibernateUtil;
+
+public class UpdateObjectTest {
+
+	public static void main(String[] args) {
+
+		//Get SessionFactory object
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		//Get Session object
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = null;
+		try(factory;ses){
+			//Prepare object
+			CallerTune tune = ses.get(CallerTune.class, 64);
+			if(tune==null) {
+				System.out.println("Record not found");
+				return;
+			}
+			else {
+				//begin transaction
+				tx = ses.beginTransaction();
+				//Update object
+				tune.setTuneName("sami sami ..");
+				ses.update(tune);
+				tx.commit();
+				System.out.println("Object is updated for :: "+tune.getUpdationCount()+" times");
+				System.out.println("Object is lastly updated at :: "+tune.getLastUpdationTime());
+			}//else
+		}//try
+		catch(HibernateException he) {
+			if(tx!=null || tx.getStatus()!=null  || tx.getRollbackOnly()) {
+				tx.rollback();
+				System.out.println("Object is not updated");
+			}//if
+			he.printStackTrace();
+		}//catch
+	}//main
+}//class
