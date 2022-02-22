3b3e0ac114d7f3191febf60bb08a6bb07cd7b8db
deepa <deepa@DESKTOP-AJQTNNR>
2022-02-22 11:30:39 +0530

    HBProj20-HBGenerators-UUID-GUID

---
+++ HBProj20-HBGenerators-UUID-GUID/src/main/java/com/nit/test/SaveObjectTestForClusteredEnv.java
@@ -0,0 +1,54 @@
+package com.nit.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.nit.entity.Product;
+import com.nit.utility.HibernateUtil;
+
+public class SaveObjectTestForClusteredEnv {
+
+	public static void main(String[] args) {
+
+		//Get SessionFactory object
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+
+		//Get Session Object
+		Session ses = HibernateUtil.getSession();
+
+		Transaction tx = null;
+		try(factory;ses){  //java9 TWR
+			//Begin Tx
+			tx = ses.beginTransaction();
+
+			//Prepare object for entity class
+			Product prod = new Product();
+			prod.setPname("Shoe Rack");
+			prod.setPrice(8000.5f);
+			prod.setQty(1.0f);
+			//instruction to save object
+			String idVal = (String) ses.save(prod);
+			System.out.println("The generated id value is :: "+idVal);
+
+			try {
+				Thread.sleep(20000);  //during this sleep time, run same app for multiple times
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			//commit the tx
+			tx.commit();
+			System.out.println("Object is saved");
+		}
+		catch(HibernateException he) {
+			if(tx!=null && tx.getStatus()!=null && tx.getRollbackOnly()) {
+				tx.rollback();
+				System.out.println("Object is not saved");
+			}
+			he.printStackTrace();
+		}
+	}//main
+}//class
---
+++ HBProj20-HBGenerators-UUID-GUID/src/main/java/com/nit/test/TicketBookingOperation.java
@@ -0,0 +1,38 @@
+package com.nit.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.nit.entity.Product;
+import com.nit.utility.HibernateUtil;
+
+public class TicketBookingOperation implements Runnable {
+
+	@Override
+	public void run() {
+		//Get Session object
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = null;
+		try(ses){
+			//Begin transaction
+			tx = ses.beginTransaction();
+			Product prod = new Product();
+			prod.setPname("Spoon");
+			prod.setPrice(900.0f);
+			prod.setQty(6.0f);
+			//Instruction to save the object
+			String idVal = (String) ses.save(prod);
+			System.out.println("Generated id value :: "+idVal);
+			tx.commit();
+			System.out.println("Object is saved");
+		}
+		catch(HibernateException he) {
+			if(tx!=null && tx.getStatus()!=null && tx.getRollbackOnly()) {
+				tx.rollback();
+				System.out.println("Object is not saved");
+			}
+			he.printStackTrace();
+		}//catch
+	}//run
+}//class
