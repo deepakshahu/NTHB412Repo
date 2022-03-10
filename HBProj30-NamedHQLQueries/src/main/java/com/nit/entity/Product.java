93650552390a915b01c158ebf9db76f59d98fe27
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-10 10:46:28 +0530

    HBProj30-NamedHQLQueries

---
+++ HBProj30-NamedHQLQueries/src/main/java/com/nit/entity/Product.java
@@ -0,0 +1,15 @@
+package com.nit.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+	private Integer pid;
+	private String pname;
+	private Float price;
+	private Float qty;
+	
+	public Product() {
+		System.out.println("Product :: 0-param constructor :: "+this.hashCode());		
+	}
+}
