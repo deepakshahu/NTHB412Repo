bc9f2b139600f54762ad825cde912a1ed734f031
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-28 12:50:28 +0530

    HBProj40-InheritanceMapping-TPCH

---
+++ HBProj40-InheritanceMapping-TPCH/src/main/java/com/nit/entity/Employee.java
@@ -0,0 +1,17 @@
+package com.nit.entity;
+
+import lombok.Data;
+
+@Data
+public class Employee extends Person {
+	
+	private String desg;
+	private Double salary;
+	private Integer deptno;
+	
+	@Override
+	public String toString() {
+		return "Employee [desg=" + desg + ", salary=" + salary + ", deptno=" + deptno + "]"+super.toString();
+	}
+	
+}
---
+++ HBProj40-InheritanceMapping-TPCH/src/main/java/com/nit/test/TPCH_InheritanceMappingInsertTest.java
@@ -0,0 +1,58 @@
+package com.nit.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.nit.entity.Customer;
+import com.nit.entity.Employee;
+import com.nit.entity.Person;
+import com.nit.utility.HibernateUtil;
+
+public class TPCH_InheritanceMappingInsertTest {
+
+	public static void main(String[] args) {
+		//Get SessionFactory, Session object
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = null;
+		try(factory;ses){
+			//begin transaction
+			tx = ses.beginTransaction();
+			//prepare objects
+			Person per = new Person();
+			per.setPname("Deepak");
+			per.setCompany("TCS");
+
+			Employee emp = new Employee();
+			emp.setPname("Harsh");
+			emp.setCompany("Wipro");
+			emp.setDesg("Developer");
+			emp.setDeptno(1001);
+			emp.setSalary(56000.0);
+
+			Customer cust = new Customer();
+			cust.setPname("Ankit");
+			cust.setCompany("SAMSUNG");
+			cust.setBillNo(2345);
+			cust.setBillAmt(9000.0);
+
+			//save objects
+			ses.save(per);
+			ses.save(emp);
+			ses.save(cust);
+
+			tx.commit();
+			System.out.println("Objects are saved");
+		}
+		catch(HibernateException he) {
+			if(tx!=null && tx.getStatus()!=null && tx.getRollbackOnly()) {
+				System.out.println("Problem in saving objects");
+				tx.rollback();
+			}
+			he.printStackTrace();
+		}
+	}
+
+}
