b4657f4059fc2a090af3a871e9a6a32dcb244afc
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-30 11:47:15 +0530

    HBProj45-InheritanceMapping-TPCH-Anno

---
+++ HBProj45-InheritanceMapping-TPCH-Anno/src/main/java/com/nit/entity/CardPayment.java
@@ -0,0 +1,21 @@
+package com.nit.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+@DiscriminatorValue("card")
+public class CardPayment extends Payment {
+	
+	private Long cardNo;
+	private String cardType;
+	private String gateway;
+	
+	@Override
+	public String toString() {
+		return "CardPayment [cardNo=" + cardNo + ", cardType=" + cardType + ", gateway=" + gateway + super.toString()+"]";
+	}
+}
---
+++ HBProj45-InheritanceMapping-TPCH-Anno/src/main/java/com/nit/entity/ChequePayment.java
@@ -0,0 +1,20 @@
+package com.nit.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+@DiscriminatorValue("cheque")
+public class ChequePayment extends Payment {
+	
+	private Long chequeNo;
+	private String chequeType;
+	
+	@Override
+	public String toString() {
+		return "ChequePayment [chequeNo=" + chequeNo + ", chequeType=" + chequeType + super.toString()+"]";
+	}
+}
