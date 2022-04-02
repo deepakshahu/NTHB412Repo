f1ec7f8d4cf3889e3e9362b326354f37fe3b293d
deepa <deepa@DESKTOP-AJQTNNR>
2022-04-02 14:41:03 +0530

    HBProj48-CollectionMapping

---
+++ HBProj48-CollectionMapping/src/main/java/com/nit/entity/PersonDetails.java
@@ -0,0 +1,22 @@
+package com.nit.entity;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDetails {
+	private Integer pid;
+	private String pname;
+	private String paddrs;
+	private List<String> nickNames;
+	private List<String> friends;
+	private Set<Long> contactNumbers;
+	private Map<String, Long> idDetails;
+}
