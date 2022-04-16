517dc27efeeb7fb115a8fedd94be20e97c0a34dc
deepa <deepa@DESKTOP-AJQTNNR>
2022-04-16 17:50:12 +0530

    HBProj54-OneToOnePK

---
+++ HBProj54-OneToOnePK/src/main/java/com/nit/entity/LibraryMembership.java
@@ -0,0 +1,26 @@
+package com.nit.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LibraryMembership implements Serializable {
+	private Integer lid;
+	private String type;
+	private LocalDate doj;
+	private Student studentDetails;
+	
+	public LibraryMembership() {
+		System.out.println("LibraryMembership :: 0-param constructor");
+	}
+
+	@Override
+	public String toString() {
+		return "LibraryMembership [lid=" + lid + ", type=" + type + ", doj=" + doj + "]";
+	}
+	
+}
---
+++ HBProj54-OneToOnePK/src/main/java/com/nit/test/OneToOnePKTest.java
@@ -0,0 +1,21 @@
+package com.nit.test;
+
+import com.nit.dao.IOneToOnePKDAO;
+import com.nit.dao.OneToOnePKDAOImpl;
+import com.nit.utility.HibernateUtil;
+
+public class OneToOnePKTest {
+
+	public static void main(String[] args) {
+		//create DAO class object
+		IOneToOnePKDAO dao = new OneToOnePKDAOImpl();
+		//dao.saveDataUsingParent();
+		//dao.saveDataUsingChild();
+		//dao.loadDataUsingParent();
+		dao.loadDataUsingChild();
+		
+		//close SessionFactory
+		HibernateUtil.closeSessionFactory();
+	}//main
+
+}//class
